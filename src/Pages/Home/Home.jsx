3cd2c6fe8b25664b7871623414fa9b5a221a8481
Hanifc56 <abuhanif007sd@gmail.com>
2024-02-01 01:44:35 +0600

    slick slider added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Footer from "../Shared/Footer";
 import Navbar from "../Shared/Navbar";
 import Banner from "./Banner";
 import NewsLetter from "./NewsLetter";
+import Promotion from "./Promotion";
 import Testimonial from "./Testimonial";
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Promotion></Promotion>
       <Testimonial></Testimonial>
       <NewsLetter></NewsLetter>
       <Footer></Footer>
